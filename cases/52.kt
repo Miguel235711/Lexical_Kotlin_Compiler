3942288958014378eeae47c2f6b93fd092fa1ddf
Miguel235711 <a01423629@itesm.mx>
2021-09-16 19:41:48 -0500

    removed e_ unnecessary files

---
+++ cases/52.kt
@@ -0,0 +1,5 @@
+val numbers = listOf(1, -2, 3, -4, 5, -6)            // 1
+
+val anyNegative = numbers.any { it < 0 }             // 2
+
+val anyGT6 = numbers.any { it > 6 }                  // 3
--- cases/e_1.kt
+++
@@ -1,6 +0,0 @@
-package org.kotlinlang.play         // 1
-
-fun main() {                        // 2
-    println("Hello, World!")        // 3
-    "fjlkf
-}
